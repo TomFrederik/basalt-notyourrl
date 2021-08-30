f7fc32885f9cbdfeab8be4338791b77ede7b69b3
TomFrederik <tlieberum@outlook.de>
2021-08-30 11:33:11 +0200

    implemented vqvae for pretraining as well as visualization. Not tested yet.

---
+++ research_code/datasets.py
@@ -0,0 +1,22 @@
+from torch.utils.data import Dataset, IterableDataset
+import minerl
+
+class VQVAEDataset(IterableDataset):
+    '''
+    For docs on BufferedBatchIter, see https://github.com/minerllabs/minerl/blob/dev/minerl/data/buffered_batch_iter.py
+    '''
+    def __init__(self, env_name, data_dir, batch_size):
+        # save batch_size
+        self.batch_size = batch_size
+        
+        # create data pipeline
+        self.data = minerl.data.make(env_name, data_dir=data_dir)
+        
+        # create iterator from pipeline
+        self.iter = minerl.data.BuffedBatchIter(self.data)       
+         
+    def __iter__(self):
+        '''
+        Returns next pov_obs in the iterator.
+        '''
+        return self.iter.buffed_batch_iter(self.batch_size)[0]['pov']
