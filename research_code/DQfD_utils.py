3e0304a52363fbb0f1a2a02d7db0e4a1882ce5a6
ddcas <ddcastilloiglesias@gmail.com>
2021-09-27 14:41:24 +0200

    finished action shaping for FindCave

--- research_code/DQfD_utils.py
+++ research_code/DQfD_utils.py
@@ -12,6 +12,14 @@ Transition = namedtuple('Transition',
 # angles of rotation above which we consider camera actions (TODO: arbitrary values, probably need tweaking)
 PITCH_MARGIN=5
 YAW_MARGIN=5
+# action priorities for each task
+ACTION_PRIORITIES = {
+    'MineRLBasaltFindCave-v0': {
+        'attack-forward-jump': 0, 'camera_down': 1, 'camera_left': 1,
+        'camera_right': 1, 'camera_up': 1, 'afj-down': 2, 'afj-left': 2,
+        'afj-right': 2, 'afj-up': 2, 'equip': 3, 'use': 4
+    }
+}
 
 class ReplayBuffer(object):
 
@@ -242,23 +250,26 @@ def loss_function(
 Define the action shaping functions, which take the original action
 and map it to the shaped action
 '''
-def get_cam_actions_shaped(delta_pitch, delta_yaw, pitch_margin, yaw_margin):
+def get_cam_actions_shaped(delta_pitch, delta_yaw, pitch_margin, yaw_margin,
+                           move_left, move_right, consider_move=True):
     '''
     Transform a single item camera action --> 'camera': [float, float] into a 4-item dict of
     camera actions -->  {'camera_down': bool, 'camera_left': bool, 'camera_right': bool, 'camera_up': bool}
+    If consider_move==True then it will also convert lateral movements to camera rotations
     '''
-    cam_actions_shaped = {'camera_down':False , 'camera_left':False, 'camera_right':False, 'camera_up':False}
-    if delta_pitch < -pitch_margin: # camera down
+    cam_actions_shaped = {'camera_down': False, 'camera_left': False,
+                          'camera_right': False, 'camera_up': False}
+    if delta_pitch < -pitch_margin:  # camera down
         cam_actions_shaped['camera_down'] = True
-    elif delta_pitch > pitch_margin: # camera up
+    elif delta_pitch > pitch_margin:  # camera up
         cam_actions_shaped['camera_up'] = True
     if delta_yaw < -yaw_margin:  # camera left
-        cam_actions_shaped['camera_left'] = True
-    elif delta_yaw > yaw_margin: # camera right
-        cam_actions_shaped['camera_right'] = True
+        cam_actions_shaped['camera_left'] = True or (consider_move and bool(move_left))
+    elif delta_yaw > yaw_margin:  # camera right
+        cam_actions_shaped['camera_right'] = True or (consider_move and bool(move_right))
     return cam_actions_shaped
 
-def remove_actions(actions, actions_to_remove):
+def remove_actions(actions, actions_to_remove=[]):
     '''
     Remove dict items by a given list of keys
     '''
@@ -266,31 +277,116 @@ def remove_actions(actions, actions_to_remove):
         del actions[act]
     return actions
 
-def insert_actions(actions, key_ref, actions_to_insert):
+def insert_actions(actions, actions_to_insert, key_ref=None):
     '''
     Insert OrderedDict items into another OrderedDict after a specified position given by a reference key
+    If no reference key is given, it will insert the items at the first position
     '''
     new_actions = actions.__class__()
+    if not key_ref:
+        for k, v in actions_to_insert.items():
+            new_actions[k] = v
     for key, value in actions.items():
         new_actions[key] = value
-        if key == key_ref:
-            for k,v in actions_to_insert.items():
-                new_actions[k] = v
+        if key_ref:
+            if key == key_ref:
+                for k,v in actions_to_insert.items():
+                    new_actions[k] = v
     actions.clear()
     actions.update(new_actions)
     return actions
 
-def find_cave_action(action):
-    # action['camera']=[float, float] ----> action['camera_down']= {0,1}, action['camera_left']= {0,1} , etc.
-    cam_actions_shaped = get_cam_actions_shaped(*action['camera'], PITCH_MARGIN, YAW_MARGIN)
-    # insert shaped camera actions
-    action_withcam = insert_actions(action, 'back', cam_actions_shaped)
-    # remove actions that are not needed
-    action_withcam_lean = remove_actions(action_withcam, ['sprint', 'sneak', 'camera', 'equip'])  # TODO: figure out 'equip'
-    # convert to one-hot so actions can be fed to a neural network
-    action_one_hot = np.array([int(a) for a in action_withcam_lean.values()]).astype(np.float32)
+def combine_actions(actions, ref_key, new_key, keys_to_avoid):
+    '''
+    Combines a certain number of actions into a smaller "summary" of them.
+    A reference key has to be present for the summary to be performed.
+    The new key is the name of the new dict "summary" item. The keys to avoid
+    are the keys of the input dict that are not going to be summarized
+    '''
+    if ref_key in actions:
+        new_item = {new_key: any([v for k,v in actions.items()
+                              if not k in keys_to_avoid])}
+        combined_actions = insert_actions(actions, new_item)
+    else:
+        return actions
 
-    return action_one_hot
+    return combined_actions
+
+def combine_actions_multi(actions, ref_key, new_keys, old_keys, keys_to_avoid):
+    '''
+    Same as combine_actions but for multiple items
+    '''
+    k3 = []
+    for k1, k2 in zip(new_keys, old_keys):
+        keys_to_avoid += k3
+        keys_to_avoid.remove(k2)
+        actions = combine_actions(
+            actions, ref_key=ref_key,
+            new_key=k1,
+            keys_to_avoid=keys_to_avoid)
+        k3 += [k2] + [k1]
+
+    return actions
+
+def shape_equip(actions):
+    '''
+    Shape the equip action space
+    '''
+    actions['equip'] = 0 if (actions['equip'] == 'none') else 1
+    # TODO: extend to rest of items and tasks
+
+    return actions
+
+def prioritize_actions(actions, act_prior):
+    '''
+    Prioritize actions depending on a ruleset. E.g.: if the agent rotates the
+    camera to the left at the same time that pressing "equip", we prioritize
+    the "equip" action
+    '''
+    for k,v in actions.items():
+        actions[k] = int(actions[k]) * 10**(act_prior[k] + 1)
+    max_action = max([v for v in actions.values()])
+    for k,v in actions.items():
+        actions[k] = int(actions[k] // (max_action if max_action else 1))
+
+    return actions
+
+def index_actions(actions):
+    '''
+    Return the index of the single active action that can be given in the
+    OrderedDict of shaped actions corresponding to a specific task
+    '''
+    return np.argmax([v for k, v in actions.items()])
+
+def find_cave_action(action):
+    # combine attack, forward and jump into a single action
+    action_combined = combine_actions(action, ref_key='forward', new_key='attack-forward-jump',
+                                      keys_to_avoid=['camera', 'equip', 'left', 'right', 'use'])
+    # action['camera']=[float, float] ----> action['camera_down']= {0,1}, action['camera_left']= {0,1} , etc.
+    cam_actions_shaped = get_cam_actions_shaped(*action['camera'],
+                                                PITCH_MARGIN, YAW_MARGIN,
+                                                action['left'], action['right'])
+    # insert shaped camera actions
+    action_withcam = insert_actions(action_combined, cam_actions_shaped, 'back')
+    # remove actions that are not needed
+    action_withcam_lean = remove_actions(
+        action_withcam,
+        ['attack', 'back', 'camera', 'forward', 'jump', 'left', 'right', 'sprint', 'sneak'])
+    # add equip action shaping
+    action_withcam_lean_equipped = shape_equip(action_withcam_lean)
+    # combine attack-forward-jump action with camera rotation actions
+    action_final = combine_actions_multi(
+        action_withcam_lean_equipped,
+        ref_key='attack-forward-jump',
+        new_keys=['afj-down', 'afj-left', 'afj-right', 'afj-up'],
+        old_keys=['camera_down', 'camera_left', 'camera_right', 'camera_up'],
+        keys_to_avoid=['attack-forward-jump', 'camera_down', 'camera_left',
+                       'camera_right', 'camera_up', 'equip', 'use'])
+    # prioritize actions
+    action_final_prioritized = prioritize_actions(
+        action_final, act_prior=ACTION_PRIORITIES['MineRLBasaltFindCave-v0'])
+
+    return action_final_prioritized, index_actions(action_final_prioritized)
 
 def make_waterfall_action(action):
     #TODO
