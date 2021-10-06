0fcebd41169f289a2461510aaf93f8d8bf8bdfb3
Ubuntu <lieberummaas@minerlVM.atcpznngfu5enedt1m0hiuvhcd.bx.internal.cloudapp.net>
2021-10-06 09:30:54 +0000

    added DQfD pretraining into the main code and changed config.yaml accordingly

--- rlhpd/1_pretrain.py
+++ rlhpd/1_pretrain.py
@@ -8,6 +8,9 @@ from pathlib import Path
 
 from common import utils
 
+# TODO: adapt when changing location of DQfD
+from research_code.Pretrain_DQfD import main
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run behavioral cloning pretraining with expert demos')
     parser.add_argument("-c", "--config-file", default="config.yaml",
@@ -16,10 +19,8 @@ if __name__ == '__main__':
 
     cfg = utils.load_config(options.config_file)
 
-    model_path = Path(cfg.pretrain_policy.model_path)
+    model_path = Path(cfg.pretrain_dqfd_args.model_path)
     model_path.parent.mkdir(parents=True, exist_ok=True)
     
-    print("Running behavioral cloning pretraining!")
-    print("Saving model to", model_path)
-    with open(model_path, "w") as f:
-        f.write("DUMMY MODEL")
+    print("Running DQfD pretraining!")
+    main(**vars(cfg.pretrain_dqfd_args))
