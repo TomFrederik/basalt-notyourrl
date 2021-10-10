e4b57040945246f8df3b9c37861f66c6305dc0a6
Jun Shern Chan <junshern.chan@motional.com>
2021-10-10 11:17:33 +0800

    Added test for state shaping

--- rlhpd/common/state_shaping.py
+++ rlhpd/common/state_shaping.py
@@ -12,13 +12,22 @@ INV_TO_IDX = {item: i for i, item in enumerate(all_inv_items)}
 EQUIP_TO_IDX = {item: i for i, item in enumerate(all_equip_items)}
 
 def preprocess_non_pov_obs(state):
+    """
+    Go from two dictionaries: state['equipped_items'] and state['inventory']
+    to a flattened one-hot(ish) vector of the form:
+    [
+        damage, maxDamage,                                              # Equip stats
+        isAirEquipped, isBucketEquipped, ... , isWheatSeedsEquipped,    # One-hot vector for equippable items
+        numBuckets, numCarrots, ... , numWheatSeeds,                    # Count for inventory items
+    ]
+    """
     # add damage vector to inv_obs
     inv_obs = [state['equipped_items']['mainhand']['damage'], state['equipped_items']['mainhand']['maxDamage']]
     
     # get number of inventory items to one-hot encoded 'equip' type
     inv = list(state['inventory'].values())
-    num_inv_items = len(inv) + 3 # in addition to inv items, we have 'air', 'other' and 'none'
-    equip = [0] * num_inv_items
+    num_equippable_items = len(inv) + 3 # in addition to inv items, we have 'air', 'other' and 'none'
+    equip = [0] * num_equippable_items
     equip[EQUIP_TO_IDX[state['equipped_items']['mainhand']['type']]] = 1
     
     # add equip type one-hot vector to inv_obs
@@ -46,3 +55,30 @@ class StateWrapper(gym.ObservationWrapper):
         
     def observation(self, obs):
         return preprocess_state(obs)
+
+if __name__ == "__main__":
+    # Run test
+    test_state = {
+        "equipped_items": {
+            "mainhand": {
+                "damage": 0,
+                "maxDamage": 0,
+                "type": "none"
+            }
+        },
+        "inventory": {
+            "bucket": 0,
+            "carrot": 0,
+            "cobblestone": 0,
+            "fence": 0,
+            "fence_gate": 0,
+            "snowball": 0,
+            "stone_pickaxe": 0,
+            "stone_shovel": 0,
+            "water_bucket": 0,
+            "wheat": 0,
+            "wheat_seeds": 3
+        }
+    }
+    expected_vec = np.array([0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3], dtype=np.float32)
+    assert np.all(preprocess_non_pov_obs(test_state) == expected_vec)
