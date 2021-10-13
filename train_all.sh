67b7e91383d479ac7516385c09a9e6c7e6c65104
Jun Shern Chan <junshern.chan@motional.com>
2021-10-14 02:25:50 +0800

    Training for FindCave and MakeWaterfall

---
+++ train_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# python -c 'import train_submission_code; train_submission_code.main("MakeWaterfall_small")'
+python -c 'import train_submission_code; train_submission_code.main("FindCave")'
+python -c 'import train_submission_code; train_submission_code.main("MakeWaterfall")'
--- train_submission_code.py
+++ train_submission_code.py
@@ -1,135 +1,138 @@
-# import logging
-# import os
-# from pathlib import Path
-# from shutil import copyfile
+import logging
+import os
+from pathlib import Path
+from shutil import copyfile
 
-# import coloredlogs
-# import gym
-# import minerl
-# import numpy as np
+import coloredlogs
+import gym
+import minerl
+import numpy as np
 
-# import aicrowd_helper
-# # from basalt_baselines.bc import bc_baseline
-# from utility.parser import Parser
+import aicrowd_helper
+# from basalt_baselines.bc import bc_baseline
+from utility.parser import Parser
 
-# coloredlogs.install(logging.DEBUG)
+coloredlogs.install(logging.DEBUG)
 
-# import json
+import json
 
-# from rlhpd import (DQfD_pretraining, DQfD_training, x_2_run_and_sample_clips,
-#                    x_3_train_reward)
-# from rlhpd.common import utils
+from rlhpd import (DQfD_pretraining, DQfD_training, x_2_run_and_sample_clips,
+                   x_3_train_reward)
+from rlhpd.common import utils
 
-# # You need to ensure that your submission is trained by launching less than MINERL_TRAINING_MAX_INSTANCES instances
-# MINERL_TRAINING_MAX_INSTANCES = int(os.getenv('MINERL_TRAINING_MAX_INSTANCES', 5))
-# # The dataset is available in data/ directory from repository root.
-# MINERL_DATA_ROOT = os.getenv('MINERL_DATA_ROOT', 'data/')
-# # You need to ensure that your submission is trained within allowed training time.
-# MINERL_TRAINING_TIMEOUT = int(os.getenv('MINERL_TRAINING_TIMEOUT_MINUTES', 4 * 24 * 60))
-# # You need to ensure that your submission is trained by launching less than MINERL_TRAINING_MAX_INSTANCES instances
-# MINERL_TRAINING_MAX_INSTANCES = int(os.getenv('MINERL_TRAINING_MAX_INSTANCES', 5))
+# You need to ensure that your submission is trained by launching less than MINERL_TRAINING_MAX_INSTANCES instances
+MINERL_TRAINING_MAX_INSTANCES = int(os.getenv('MINERL_TRAINING_MAX_INSTANCES', 5))
+# The dataset is available in data/ directory from repository root.
+MINERL_DATA_ROOT = os.getenv('MINERL_DATA_ROOT', 'data/')
+# You need to ensure that your submission is trained within allowed training time.
+MINERL_TRAINING_TIMEOUT = int(os.getenv('MINERL_TRAINING_TIMEOUT_MINUTES', 4 * 24 * 60))
+# You need to ensure that your submission is trained by launching less than MINERL_TRAINING_MAX_INSTANCES instances
+MINERL_TRAINING_MAX_INSTANCES = int(os.getenv('MINERL_TRAINING_MAX_INSTANCES', 5))
 
-# BASALT_GYM_ENV = os.getenv('MINERL_GYM_ENV', 'MineRLBasaltFindCave-v0')
+BASALT_GYM_ENV = os.getenv('MINERL_GYM_ENV', 'MineRLBasaltFindCave-v0')
 
 
-# def main():
-#     with open("aicrowd.json",) as f:
-#         ai_crowd_json = json.load(f)
-#     if ai_crowd_json["tags"] == "BuildVillageHouse":
-#         cfg = utils.load_config("rlhpd/config_BuildVillageHouse.yaml")
-#     elif ai_crowd_json["tags"] == "MakeWaterfall":
-#         cfg = utils.load_config("rlhpd/config_MakeWaterfall.yaml")
-#     elif ai_crowd_json["tags"] == "FindCave":
-#         cfg = utils.load_config("rlhpd/config_FindCave.yaml")
-#     elif ai_crowd_json["tags"] == "VillageAnimalPen":
-#         cfg = utils.load_config("rlhpd/config_VillageAnimalPen.yaml")
-#     elif ai_crowd_json["tags"] == "MakeWaterfall_small":
-#         cfg = utils.load_config("rlhpd/config_MakeWaterfall_small.yaml")
-#     else: 
-#         Exception("tag is wrong")
-#     model_savepath = (Path("train") / cfg.env_task).with_suffix(".pt")
+def main(task_name = None):
+    if task_name is None:
+        with open("aicrowd.json",) as f:
+            ai_crowd_json = json.load(f)
+            task_name = ai_crowd_json["tags"]
     
-#     # Pretrain
-#     print("Running DQfD pretraining!")
-#     args = {**vars(cfg.pretrain_dqfd_args), **vars(cfg.dqfd_args)} # join common dqfd args with those that are specific for pretraining
-#     DQfD_pretraining.main(**args)
-#     copyfile(cfg.pretrain_dqfd_args.model_path, model_savepath)
+    if task_name == "BuildVillageHouse":
+        cfg = utils.load_config("rlhpd/config_BuildVillageHouse.yaml")
+    elif task_name == "MakeWaterfall":
+        cfg = utils.load_config("rlhpd/config_MakeWaterfall.yaml")
+    elif task_name == "FindCave":
+        cfg = utils.load_config("rlhpd/config_FindCave.yaml")
+    elif task_name == "VillageAnimalPen":
+        cfg = utils.load_config("rlhpd/config_VillageAnimalPen.yaml")
+    elif task_name == "MakeWaterfall_small":
+        cfg = utils.load_config("rlhpd/config_MakeWaterfall_small.yaml")
+    else: 
+        Exception("tag is wrong")
+    model_savepath = (Path("train") / cfg.env_task).with_suffix(".pt")
+    
+    # Pretrain
+    print("############################## Running DQfD pretraining!")
+    args = {**vars(cfg.pretrain_dqfd_args), **vars(cfg.dqfd_args)} # join common dqfd args with those that are specific for pretraining
+    DQfD_pretraining.main(**args)
+    copyfile(cfg.pretrain_dqfd_args.model_path, model_savepath)
+    print(f"Copied {cfg.pretrain_dqfd_args.model_path} to {model_savepath}")
 
-#     # Run and sample
-#     print("Sampling clips")
-#     x_2_run_and_sample_clips.main(cfg)
+    # Run and sample
+    print("############################## Sampling clips")
+    x_2_run_and_sample_clips.main(cfg)
 
-#     # Train reward
-#     print("Training reward!")
-#     x_3_train_reward.main(cfg)
+    # Train reward
+    print("############################## Training reward!")
+    x_3_train_reward.main(cfg)
 
-#     # Train DQfD
-#     print("Running DQfD training!")
-#     args = {**vars(cfg.train_dqfd_args), **vars(cfg.dqfd_args)} # join common dqfd args with those that are specific for training
-#     DQfD_training.main(**args)
+    # Train DQfD
+    print("############################## Running DQfD training!")
+    args = {**vars(cfg.train_dqfd_args), **vars(cfg.dqfd_args)} # join common dqfd args with those that are specific for training
+    DQfD_training.main(**args)
+    copyfile(cfg.train_dqfd_args.new_model_path, model_savepath)
+    print(f"Copied {cfg.train_dqfd_args.new_model_path} to {model_savepath}")
 
-def main():
-    print("Skipping training")
-
+    print(f"Training for {task_name} ran successfully!")
 
 if __name__ == "__main__":
     main()
 
-
-# # # Optional: You can view best effort status of your instances with the help of parser.py
-# # # This will give you current state like number of steps completed, instances launched and so on.
-# # # Make your you keep a tap on the numbers to avoid breaching any limits.
-# # parser = Parser(
-# #     'performance/',
-# #     maximum_instances=MINERL_TRAINING_MAX_INSTANCES,
-# #     raise_on_error=False,
-# #     no_entry_poll_timeout=600,
-# #     submission_timeout=MINERL_TRAINING_TIMEOUT * 60,
-# #     initial_poll_timeout=600
-# # )
-
-
-# # def basic_train():
-# #     """
-# #     This function will be called for training phase.
-# #     This should produce and save same files you upload during your submission.
-# #     """
-# #     # How to sample minerl data is document here:
-# #     # http://minerl.io/docs/tutorials/data_sampling.html
-# #     data = minerl.data.make('MineRLBasaltFindCave-v0', data_dir=MINERL_DATA_ROOT)
-
-# #     # Sample code for illustration, add your training code below
-# #     env = gym.make('MineRLBasaltFindCave-v0')
-
-# #     # For an example, lets just run one episode of MineRL for training
-# #     obs = env.reset()
-# #     done = False
-# #     while not done:
-# #         obs, reward, done, info = env.step(env.action_space.sample())
-# #         # Do your training here
-
-# #         # To get better view in your training phase, it is suggested
-# #         # to register progress continuously, example when 54% completed
-# #         # aicrowd_helper.register_progress(0.54)
-
-# #         # To fetch latest information from instance manager, you can run below when you want to know the state
-# #         #>> parser.update_information()
-# #         #>> print(parser.payload)
-
-# #     # Save trained model to train/ directory
-# #     # For a demonstration, we save some dummy data.
-# #     np.save("./train/parameters.npy", np.random.random((10,)))
-
-# #     # Training 100% Completed
-# #     aicrowd_helper.register_progress(1)
-# #     env.close()
+# # Optional: You can view best effort status of your instances with the help of parser.py
+# # This will give you current state like number of steps completed, instances launched and so on.
+# # Make your you keep a tap on the numbers to avoid breaching any limits.
+# parser = Parser(
+#     'performance/',
+#     maximum_instances=MINERL_TRAINING_MAX_INSTANCES,
+#     raise_on_error=False,
+#     no_entry_poll_timeout=600,
+#     submission_timeout=MINERL_TRAINING_TIMEOUT * 60,
+#     initial_poll_timeout=600
+# )
 
 
-# # def main():
-# #     # Documentation for BC Baseline can be found in train_bc.py
-# #     # TODO make this configurable once we have multiple baselines
-# #     TRAINING_EXPERIMENT = bc_baseline
-# #     TRAINING_EXPERIMENT.run(config_updates={'data_root': MINERL_DATA_ROOT,
-# #                                             'task_name': BASALT_GYM_ENV,
-# #                                             'train_batches': 10,
-# #                                             'save_dir': "train"})
+# def basic_train():
+#     """
+#     This function will be called for training phase.
+#     This should produce and save same files you upload during your submission.
+#     """
+#     # How to sample minerl data is document here:
+#     # http://minerl.io/docs/tutorials/data_sampling.html
+#     data = minerl.data.make('MineRLBasaltFindCave-v0', data_dir=MINERL_DATA_ROOT)
+
+#     # Sample code for illustration, add your training code below
+#     env = gym.make('MineRLBasaltFindCave-v0')
+
+#     # For an example, lets just run one episode of MineRL for training
+#     obs = env.reset()
+#     done = False
+#     while not done:
+#         obs, reward, done, info = env.step(env.action_space.sample())
+#         # Do your training here
+
+#         # To get better view in your training phase, it is suggested
+#         # to register progress continuously, example when 54% completed
+#         # aicrowd_helper.register_progress(0.54)
+
+#         # To fetch latest information from instance manager, you can run below when you want to know the state
+#         #>> parser.update_information()
+#         #>> print(parser.payload)
+
+#     # Save trained model to train/ directory
+#     # For a demonstration, we save some dummy data.
+#     np.save("./train/parameters.npy", np.random.random((10,)))
+
+#     # Training 100% Completed
+#     aicrowd_helper.register_progress(1)
+#     env.close()
+
+
+# def main():
+#     # Documentation for BC Baseline can be found in train_bc.py
+#     # TODO make this configurable once we have multiple baselines
+#     TRAINING_EXPERIMENT = bc_baseline
+#     TRAINING_EXPERIMENT.run(config_updates={'data_root': MINERL_DATA_ROOT,
+#                                             'task_name': BASALT_GYM_ENV,
+#                                             'train_batches': 10,
+#                                             'save_dir': "train"})
