e8290be0a057422084fb76f8140e8c364bed89f8
Jun Shern Chan <junshern.chan@motional.com>
2021-10-14 19:02:57 +0800

    Disable snowball in findcave and makewaterfall

---
+++ generate_videos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ENV_NAME=MineRLBasaltFindCave-v0
+NUM_EPISODES=5
+MODEL=Q_0
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL}_nosnowball --num_episodes $NUM_EPISODES
+
+
+ENV_NAME=MineRLBasaltFindCave-v0
+NUM_EPISODES=5
+MODEL=Q_1
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL}_nosnowball --num_episodes $NUM_EPISODES
+
+
+ENV_NAME=MineRLBasaltMakeWaterfall-v0
+NUM_EPISODES=5
+MODEL=Q_0
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL}_nosnowball --num_episodes $NUM_EPISODES
+
+
+ENV_NAME=MineRLBasaltMakeWaterfall-v0
+NUM_EPISODES=5
+MODEL=Q_1
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL}_nosnowball --num_episodes $NUM_EPISODES
