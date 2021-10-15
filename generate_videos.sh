e812f677486d8b5aa60e184d9b3d5b8729db16a9
Jun Shern Chan <junshern.chan@motional.com>
2021-10-15 18:56:55 +0800

    Update generate_videos script to use the trained submission models

--- generate_videos.sh
+++ generate_videos.sh
@@ -3,28 +3,13 @@
 NUM_EPISODES=5
 
 ENV_NAME=MineRLBasaltFindCave-v0
-MODEL=Q_0
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-MODEL=Q_1
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path train/$ENV_NAME.pt --video_dir output/videos/$ENV_NAME --num_episodes $NUM_EPISODES
 
 ENV_NAME=MineRLBasaltMakeWaterfall-v0
-MODEL=Q_0
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-MODEL=Q_1
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path train/$ENV_NAME.pt --video_dir output/videos/$ENV_NAME --num_episodes $NUM_EPISODES
 
 ENV_NAME=MineRLBasaltBuildVillageHouse-v0
-MODEL=Q_0
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-MODEL=Q_1
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path train/$ENV_NAME.pt --video_dir output/videos/$ENV_NAME --num_episodes $NUM_EPISODES
 
 ENV_NAME=MineRLBasaltCreateVillageAnimalPen-v0
-MODEL=Q_0
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
-MODEL=Q_1
-python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path output/$ENV_NAME/$MODEL.pth --video_dir output/$ENV_NAME/videos/${MODEL} --num_episodes $NUM_EPISODES
+python rlhpd/DQfD_eval_pretrained.py --env_name $ENV_NAME  --model_path train/$ENV_NAME.pt --video_dir output/videos/$ENV_NAME --num_episodes $NUM_EPISODES
